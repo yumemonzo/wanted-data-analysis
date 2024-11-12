c3d147c07dabe9a9d3d8564ab5e1be000089b0de
yumemonzo <yumemonzo@gmail.com>
2024-11-12 02:08:29 +0000

    Bug: Fix headless option bug

--- crawling.py
+++ crawling.py
@@ -19,8 +19,11 @@ def load_job_name():
 
 def setup_driver():
     options = Options()
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.20 Safari/537.36")
     options.add_argument("--lang=ko-KR")
     driver = webdriver.Chrome(options=options)
 
